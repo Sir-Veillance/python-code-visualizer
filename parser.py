23df11e6a04dc74802e000ef04699155ba3a5e26
Sir-Veillance <bmckay900@gmail.com>
2021-01-03 20:07:54 -0500

    Adds an implementation for returning a representation of a Python script. Most ast objects are not implemented yet, so it only interprets class and function definitions. It is useful as a proof of concept, but not complete yet.

--- parser.py
+++ parser.py
@@ -1,14 +1,62 @@
 import ast
 
+# filename = input("Enter filepath: ")
+filename = "sample_code.py"
 
-with open("sample_code.py") as f:
-    file_contents = f.read()
-test_object = ast.parse(file_contents)
+try:
+    with open(filename) as f:
+        file_contents = f.read()
+    tree = ast.parse(file_contents)
+except FileNotFoundError:
+    tree = None
+    print("File not found with given filepath.")
+    input("Press any key to exit...")
+    quit()
 
-for node in test_object.body:
-    if type(node) == ast.FunctionDef:
-        print(f"Found a function definition for function named: {node.name}")
-        print("  Function arguments:")
-        for argument in node.args.args:
-            print(f"    {argument.arg}")
 
+def unpack(node):
+    """
+    unpack takes an ast.Module object given from ast.parse() and returns a string giving a representation of
+    the structure.
+
+    Parameters:
+        node (ast.AST): The original call to unpack should pass an ast.Module object generated from ast.parse()
+                        unpack will then recursively call itself with each element from the original ast.Module
+
+    Returns:
+        string
+    """
+    # if/elseif structure for ast object cases
+    if type(node) == ast.Module:
+        s = f"Unpacked representation:"
+        for element in node.body:
+            for line in unpack(element).splitlines():
+                s += f"\n  {line}"
+        return s
+    elif type(node) == ast.ClassDef:
+        s = f"Class definition -> {node.name}"
+        if len(node.bases) > 0:
+            s += f"\nBase classes:"
+            for base in node.bases:
+                s += f"\n  {base}"
+        for element in node.body:
+            for line in unpack(element).splitlines():
+                s += f"\n  {line}"
+        return s
+    elif type(node) == ast.FunctionDef:
+        s = f"Function definition -> {node.name}"
+        if len(node.args.args) > 0 or node.args.vararg is not None:
+            s += f"\n  arguments:"
+            for argument in node.args.args:
+                s += f"\n    {argument.arg}"
+            if node.args.vararg is not None:
+                s += f"\n    *{node.args.vararg.arg}"
+        for element in node.body:
+            for line in unpack(element).splitlines():
+                s += f"\n  {line}"
+        return s
+    else:
+        return "n/a"
+
+
+print(unpack(tree))
